b130416f12c3fc5f5ac294426d0bb229cc2027b1
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-03-17 23:31:29 +0530

    made Product.java for seting product name and quantity

---
+++ src/Product.java
@@ -0,0 +1,13 @@
+public class Product {
+    String name;
+    int quantity;
+
+    void setData(String n, int q) {
+        name = n;
+        quantity = q;
+    }
+
+    void display() {
+        System.out.println(name + " - " + quantity);
+    }
+}
