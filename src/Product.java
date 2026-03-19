98974c923699510e97277192b8b067e6edaf7084
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-03-19 22:31:48 +0530

    updated the Product.java class for storing price and total

--- src/Product.java
+++ src/Product.java
@@ -2,13 +2,23 @@ class Product {
 
     String name;
     int quantity;
+    double price;
 
-    void setData(String n, int q) {
+    void setData(String n, int q, double p) {
         name = n;
         quantity = q;
+        price = p;
     }
 
     void display() {
-        System.out.println(name + " - " + quantity);
+
+        double total = quantity * price;
+
+        System.out.println(
+                name +
+                        " | Qty: " + quantity +
+                        " | Price: " + price +
+                        " | Total: " + total
+        );
     }
 }
